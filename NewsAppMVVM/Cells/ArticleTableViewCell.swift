7fe44a5c8ad05f592aebadb360ea3902ed43b63d
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-09 02:57:05 +0100

    [News]: Populating and displaying news in the tableview

---
+++ NewsAppMVVM/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleTableViewCell.swift
+//  NewsAppMVVM
+//
+//  Created by Òscar Muntal on 9/3/23.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+}
--- NewsAppMVVM/Controllers/NewsTableViewController.swift
+++ NewsAppMVVM/Controllers/NewsTableViewController.swift
@@ -9,12 +9,34 @@ import UIKit
 import RxSwift
 
 class NewsTableViewController: UITableViewController {
-    let disposeBag = DisposeBag()
+    private let disposeBag = DisposeBag()
+    private var articleListViewModel: ArticleListViewModel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.prefersLargeTitles = true
         populateNews()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let articleListVM = articleListViewModel else { return 0 }
+        return articleListVM.articles.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as? ArticleTableViewCell else { fatalError("ArticleTableViewCell not found") }
+        
+        guard let articleVM = articleListViewModel?.articleAt(indexPath.row) else { return cell }
+        
+        articleVM.title.asDriver(onErrorJustReturn: "")
+            .drive(cell.titleLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        articleVM.description.asDriver(onErrorJustReturn: "")
+            .drive(cell.descriptionLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        return cell
     }
 }
 
@@ -25,8 +47,15 @@ private extension NewsTableViewController {
         let resource = Resource<ArticleResponse>(url: url)
         
         URLRequest.requestDecodable(resource: resource)
-            .subscribe(onNext: {
-                print($0)
+            .subscribe(onNext: { articleResponse in
+                
+                let articles = articleResponse.articles
+                self.articleListViewModel = ArticleListViewModel(articles)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+                
             }).disposed(by: disposeBag)
     }
 }
