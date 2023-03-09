34c927b4ef4f3672b06eab18252f42de572fcad7
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-09 01:47:42 +0100

    [Model]: Implementing the models

---
+++ NewsAppMVVM/Models/Article.swift
@@ -0,0 +1,17 @@
+//
+//  Article.swift
+//  NewsAppMVVM
+//
+//  Created by Òscar Muntal on 9/3/23.
+//
+
+import Foundation
+
+struct ArticleResponse: Decodable {
+    let articles: [Article]
+}
+
+struct Article: Decodable {
+    let title: String
+    let description: String
+}
